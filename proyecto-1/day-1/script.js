b1485a741e62332e4c5483841d7196f6247ab5d2
Carlos Andres Alzate <un.artbliss@gmail.com>
2018-06-28 10:21:07 -0300

    Initial Commit

---
+++ proyecto-1/day-1/script.js
@@ -0,0 +1,25 @@
+// mostrar el numero de la tecla 
+// Si agrego en console.log e.keyCode puedo ver el numero
+// 	// window.addEventListener('keydown', function(e) {
+// 	// console.log(e);
+// });
+
+ // var celsius = 9/5 + 32;
+
+ // function convertToF(celsius) {
+ // 	console.log(celsius);
+ // }
+ // console.log(celsius);
+
+
+ function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  var result = "My " + myNoun + " is very " + myAdjective + " he doesn't " + myVerb " never " myAdverb;
+  // Your code below this line
+  
+  
+  // Your code above this line
+  return result;
+}
+
+// Change the words here to test your function
+wordBlanks("dog", "big", "ran", "quickly");
